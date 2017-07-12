809a835a7055663f801fbd7b0851e70a3dbbf738
Diego <diekus@gmail.com>
2017-07-12 12:28:10 +0100

    adds oscillator sound

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 var audioCtx = null;
+var tag = null;
+var osc = null;
 
 document.addEventListener('DOMContentLoaded', function() {
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
+    setupTag();
+
+
     document.getElementById('btnTag').addEventListener('click', function(){
-        playTag();
+        console.log('playing media element ');
+        playTag();      
     });
 
     document.getElementById('btnMic').addEventListener('click', function(){
@@ -12,13 +18,14 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.getElementById('btnOsc').addEventListener('click', function(){
-        console.log('Osc');
+        console.log('playing ocsillator');
+        playOsc();
     });
 });
 
-var playTag = function(){
+var setupTag = function(){
     //gets the tag with the media element
-    var tag = document.getElementById('violin');
+    tag = document.getElementById('violin');
     //creates a source based on this element
     var src = audioCtx.createMediaElementSource(tag);
     //gain node for volume
@@ -27,12 +34,16 @@ var playTag = function(){
     //connects nodes
     src.connect(gainNode);
     gainNode.connect(audioCtx.destination);
-    //plays audio
-    tag.play();
+};
 
+var playTag = function(){
+    tag.play();
 };
 
 var playOsc = function(){
-
+    osc = audioCtx.createOscillator();
+    osc.frequency.value = 150;
+    osc.connect(audioCtx.destination);
+    osc.start(audioCtx.currentTime);
+    osc.stop(audioCtx.currentTime + .5);
 };
-
