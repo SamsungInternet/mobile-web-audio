cbd46e6db0ebe1563ada1cadea2f484aa2438533
Diego <diekus@gmail.com>
2017-07-12 11:15:12 +0100

    adds buttons for different type of input

--- js/app.js
+++ js/app.js
@@ -2,4 +2,17 @@ var audioCtx = null;
 
 document.addEventListener('DOMContentLoaded', function() {
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    document.getElementById('btnTag').addEventListener('click', function(){
+        console.log('Tag');
+    });
+
+    document.getElementById('btnMic').addEventListener('click', function(){
+        console.log('Mic');
+    });
+
+    document.getElementById('btnOsc').addEventListener('click', function(){
+        console.log('Osc');
+    });
 });
+
